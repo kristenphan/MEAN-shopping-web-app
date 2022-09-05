f15385dcf5016d30d4201575d03fa1c2257711e0
Kristen Phan <hgiang.phan@yahoo.com.vn>
2022-09-05 21:57:04 +0200

    deals.js: invoke dbAbstractionLayer.queryDealsCollection() to get json response which is returned to angular UI HomeComponent

--- market-service/routes/deals.js
+++ market-service/routes/deals.js
@@ -1,23 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var dbAbstractionLayer = require('../public/javascripts/dbAbstractionLayer');
 
-/* GET users listing. */
+/* GET card details */
 router.get('/', function(req, res, next) {
-	let jsonResponse = {
-		"handsetCards": [
-			{ imageName: 'offer1', title: '10% off on personal cares', cols: 2, rows: 1 },
-			{ imageName: 'offer2', title: 'Flash sales in Footwears', cols: 2, rows: 1 },
-			{ imageName: 'offer3', title: 'Extended warranty for Apple products', cols: 2, rows: 1 },
-			{ imageName: 'offer4', title: '5% discount for grocery', cols: 2, rows: 1 }
-		],
-		"webCards": [
-			{ imageName: 'offer1', title: '10% off on personal cares', cols: 2, rows: 1 },
-			{ imageName: 'offer2', title: 'Flash sales in Footwears', cols: 1, rows: 1 },
-			{ imageName: 'offer3', title: 'Extended warranty for Apple products', cols: 1, rows: 2 },
-			{ imageName: 'offer4', title: '5% discount for grocery', cols: 1, rows: 1 }
-		]
-	}
-	res.json(jsonResponse);
+	// Fetch data from mongodb and return a json object to angular UI
+	dbAbstractionLayer.queryDealsCollection().then(response => {
+			res.json(response);
+		}).catch(error => { // return empty json object with http code 500
+			res.json.status(500).json({});
+		});
+
 });
 
 module.exports = router;
