8c1a1a2bff6fbfbbe7729f6e6721c64849c2709a
Kristen Phan <hgiang.phan@yahoo.com.vn>
2022-09-05 15:40:18 +0200

    deals.js: add imageNamel update title

--- market-service/app.js
+++ market-service/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 app.use('/', indexRouter);
-app.use('/dealsx', dealsRouter);
+app.use('/deals', dealsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
