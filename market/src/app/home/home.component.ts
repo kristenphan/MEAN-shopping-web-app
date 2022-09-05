a3dddcba7e0b96994c3ace292ea820a08dba03a9
Kristen Phan <hgiang.phan@yahoo.com.vn>
2022-09-05 14:45:16 +0200

    1) app.service.ts: create augular service; add getDeals() to call GET method from market-service/routes/deals.js; 2) home.component.ts: add appService in HomeComponent's constructor; create empty cards[] to hold cards detail from GET call; load appropriate cards when screen size changes (handset vs. web) 3) home.component.html: change "cards.cols" to "cards['cols']" to resolve error Property 'cols' does not exist on type 'never' 4) app.module.ts: add HttpClient and HttpClientModule in "import" statement in "imports" section 5) market-service/app.js: set up cors service to skip cors check. otherwise, GUI localhost:4200/home will not be able to access localhost:3000/deals for the GUI to get cards details for home dashboard

--- market/src/app/home/home.component.ts
+++ market/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-home',
@@ -9,25 +11,42 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 })
 export class HomeComponent {
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  cards = [];
+  cardsForHandset = [];
+  cardsForWeb = [];
+
+  isHandset:boolean = false;
+  isHandsetObserver: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
-        return [
-          { title: 'Card 1', cols: 2, rows: 1 },
-          { title: 'Card 2', cols: 2, rows: 1 },
-          { title: 'Card 3', cols: 2, rows: 1 },
-          { title: 'Card 4', cols: 2, rows: 1 }
-        ];
+        return true;
       }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
+      return false;
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,
+              public appService: AppService) {}
+
+  ngOnInit() {
+    this.isHandsetObserver.subscribe(currentObserverValue => {
+      this.isHandset = currentObserverValue;
+      this.loadCards();
+    });
+
+    this.appService.getDeals().subscribe(
+      response => {
+        this.cardsForHandset = response.handsetCards;
+        this.cardsForWeb = response.webCards;
+        this.loadCards();
+      },
+      error => {
+
+      }
+    );
+  }
+
+  loadCards() {
+    this.cards = this.isHandset? this.cardsForHandset : this.cardsForWeb;
+  }
 }
